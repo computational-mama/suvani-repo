f1586f060b33fcf15df9b5d73286c0409f526b89
ambikajo <ambikajoshi@gmail.com>
2021-07-22 22:00:57 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,30 +1,40 @@
+
 var foo = new p5.SpeechRec(); // speech recognition object (will prompt for mic access)
-// foo.onResult = showResult; // bind callback function to trigger when speech is recognized
-let button
-// conf = foo.resultConfidence;
+let button;
+let clang;
 
-function setup(){
-    createCanvas(700,400)
-    button =  createButton('start')
-    button.mousePressed(startMic)
+function preload(){
+  clang = createAudio("https://raw.githubusercontent.com/computational-mama/suvani-repo/main/2-spring.wav")
+}
+function setup() {
+	createCanvas(300, 300);
+	button =  createButton('start')
+  button.mousePressed(startMic)
+    // button.position(width/2,300)
+
 }
 
-function draw(){
-    background(255)
-
+function draw() {
+	background(200)
+  if(foo.resultString != ""){
+        text(foo.resultString,width/2,height/2)
+          if(foo.resultConfidence<0.6){
+            clang.play()
+          }
+    }
 }
-
 
 function startMic(){
     console.log("mic on")
-    foo.continuous = true;
+    // foo.continuous = true;
     foo.interimResults = true;
     foo.onResult = showResult;
     foo.start(); // start listening
+    
 }
 
 function showResult()
 {
   console.log(foo.resultString, foo.resultConfidence); // log the result
-  text(foo.resultString)
+  //text(foo.resultString)
 }
